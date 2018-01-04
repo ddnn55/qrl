6f50aa5e29df04ccc35fb0753bd2b15d707a9b07
David Stolarsky <david.stolarsky@gmail.com>
2018-01-04 01:37:45 -0500

    use filesystem

--- qrl.js
+++ qrl.js
@@ -2,32 +2,11 @@
 
 const QRCode = require('qrcode');
 const argv = require('yargs').argv;
-var express = require('express')
-var app = express()
-const getPort = require('get-port');
 const open = require('open');
+const tempfile = require('tempfile');
+
+const path = tempfile('.png');
  
-getPort().then(port => {
-
-    app.get('/', function (req, res) {
-        QRCode.toDataURL(argv._[0])
-        .then(url => {
-            res.send(`<img src="${url}"/>`);
-            setTimeout(function() {
-                process.exit(0);
-            }, 3000);
-        })
-        .catch(err => {
-            res.status(500);
-            res.send('Oops something went wrong.');
-            console.error('Oops something went wrong');
-            console.error(err);
-            process.exit(1);
-        })
-    });
-
-    app.listen(port, function () {
-        open('http://localhost:' + port);
-    });
-    
+QRCode.toFile(path, argv._[0], function(err, result) {
+    open(path);
 });
